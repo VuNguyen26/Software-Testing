51c5086e7e05caf069fa621e6b601fc5d16487a2
vunguyen26 <nguyenminhvu591@gmail.com>
2025-11-07 10:09:07 +0700

    chore(ci): enable Cypress video & screenshot artifacts

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:4173",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
+  video: true,
+  screenshotOnRunFailure: true,
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
+  chromeWebSecurity: false,
+  retries: 1,
 });
